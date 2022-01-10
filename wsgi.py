4701739638f196aa13ea29107b36551fa2d4a53a
Magali Rousseau <rousseaumagali@gmail.com>
2022-01-10 11:44:30 +0400

    implemented products test and fixed produts error

--- wsgi.py
+++ wsgi.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 PRODUCTS = {
     1: { 'id': 1, 'name': 'Skello' },
     2: { 'id': 2, 'name': 'Socialive.tv' },
+    3: { 'id': 3, 'name': 'Le Wagon'},
 }
 
 @app.route('/')
@@ -16,4 +17,5 @@ def hello():
 
 @app.route('/api/v1/products')
 def products():
-    return jsonify(PRODUCTS)
+    products = list(PRODUCTS.values())
+    return jsonify(products)
